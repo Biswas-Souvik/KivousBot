78fac02251d8a2af052784f1f241131a4183d390
Souvik Biswas <Biswassouvik284@gmail.com>
2022-08-15 18:19:45 +0530

    Add files via upload

---
+++ Model/chat.py
@@ -0,0 +1,60 @@
+import torch.nn as nn
+import json, torch, random
+from nltk_util import bag_of_words, tokenize
+
+with open('app/bot_data_1_0.json', 'r') as f:
+    intents_data = json.load(f)
+
+class NeuralNet(nn.Module):
+    def __init__(self, input_size, hidden_size, num_classes):
+        super(NeuralNet, self).__init__()
+        self.l1 = nn.Linear(input_size, hidden_size)
+        self.l2 = nn.Linear(hidden_size, hidden_size)
+        self.l3 = nn.Linear(hidden_size, num_classes)
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        out = self.l1(x)
+        out = self.relu(out)
+        out = self.l2(out)
+        out = self.relu(out)
+        out = self.l3(out)
+        #no activation and no softmax
+        return out
+
+
+FILE = 'app/data_1_0.pth'
+data = torch.load(FILE)
+
+input_size = data['input_size']
+hidden_size = data['hidden_size']
+output_size = data['output_size']
+all_words = data['all_words']
+tags = data['tags']
+model_state = data['model_state']
+
+model = NeuralNet(input_size, hidden_size, output_size)
+model.load_state_dict(model_state)
+model.eval()
+
+bot_name = 'kivous'
+
+def get_response(msg):
+    sentence = tokenize(msg)
+    X = bag_of_words(sentence, all_words)
+    X = X.reshape(1, X.shape[0])
+    X = torch.from_numpy(X)
+
+    output= model(X)
+    _, predicted = torch.max(output, dim=1)
+    tag = tags[predicted.item()]
+
+    probs = torch.softmax(output, dim=1)
+    prob = probs[0][predicted.item()]
+
+    if prob.item() > 0.75:
+        for intent in intents_data['intents']:
+            if tag == intent['tag']:
+                return random.choice(intent['responses'])
+    else:
+        return "I understand ψ(._. )>"
