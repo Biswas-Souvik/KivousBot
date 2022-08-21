99d47523325b6735f5596d7b35f63d79942a59fb
Souvik Biswas <Biswassouvik284@gmail.com>
2022-08-21 13:57:53 +0530

    Update chat.py

--- app/chat.py
+++ app/chat.py
@@ -1,9 +1,9 @@
 import torch.nn as nn
 import json, torch, random
-from nltk_util import bag_of_words, tokenize
+from nltk_util import tokenize, stem, tf_idf
 
-with open('app/bot_data_1_0.json', 'r') as f:
-    intents_data = json.load(f)
+f = open('app/bot_data.json',encoding="utf8")
+intents_data = json.load(f)
 
 class NeuralNet(nn.Module):
     def __init__(self, input_size, hidden_size, num_classes):
@@ -22,10 +22,11 @@ class NeuralNet(nn.Module):
         #no activation and no softmax
         return out
 
-
-FILE = 'app/data_1_0.pth'
+# load data of Pretrained Model
+FILE = 'app/bot_model.pth'
 data = torch.load(FILE)
 
+# importing the hyperparameters
 input_size = data['input_size']
 hidden_size = data['hidden_size']
 output_size = data['output_size']
@@ -33,28 +34,47 @@ all_words = data['all_words']
 tags = data['tags']
 model_state = data['model_state']
 
+# initialize and load the model
 model = NeuralNet(input_size, hidden_size, output_size)
 model.load_state_dict(model_state)
 model.eval()
 
+# give a bot name
 bot_name = 'kivous'
 
+# define the function which will return responses
 def get_response(msg):
-    sentence = tokenize(msg)
-    X = bag_of_words(sentence, all_words)
+    #Prepare the message data
+    msg_tokens = tokenize(msg)
+    msg_tokens = [stem(w) for w in msg_tokens]
+    # X = bag_of_words(sentence, all_words)
+    X = tf_idf(msg_tokens, all_words)
+
     X = X.reshape(1, X.shape[0])
     X = torch.from_numpy(X)
 
+    # Classify message to one of the tags
     output= model(X)
     _, predicted = torch.max(output, dim=1)
     tag = tags[predicted.item()]
 
+    # probability of being in that class
     probs = torch.softmax(output, dim=1)
     prob = probs[0][predicted.item()]
 
+    # define statements in case the classification probabilty is too low
+    error_text= [
+        "I'm sorry, I don't understand (._. )>",
+        "I only could understand a little (⊙_⊙)?", 
+        "Dont know that yet (。_。)", 
+        "sorry could not understand ¯\(°_o)/¯",
+        "I'm confused... (⊙ˍ⊙)"
+    ]
+
+    # return a random response 
     if prob.item() > 0.75:
         for intent in intents_data['intents']:
             if tag == intent['tag']:
-                return random.choice(intent['responses'])
+                return random.choice(intent['responses']), prob.item(), tag
     else:
-        return "I understand ψ(._. )>"
+        return random.choice(error_text), prob.item(), tag
